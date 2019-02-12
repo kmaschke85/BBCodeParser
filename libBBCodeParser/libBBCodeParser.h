301aecec9fe76fab2480d6b6878be8fb1ac6db21
Kai Maschke <>
2019-02-12 15:25:19 +0100

    Added project to build static library

---
+++ libBBCodeParser/libBBCodeParser.h
@@ -0,0 +1,13 @@
+//
+//  libBBCodeParser.h
+//  BBCodeParser
+//
+//  Created by Kai Maschke on 12.02.19.
+//  Copyright © 2019 Limeade. All rights reserved.
+//
+
+#import "BBAttribute.h"
+#import "BBCodeParser.h"
+#import "BBCodeParserDelegate.h"
+#import "BBElement.h"
+#import "BBParsingElement.h"
